d116ed091a395f7be5bee00e077e5ae67303e93f
unknown <jp.gomugomuborgs@gmail.com>
2025-08-14 03:25:05 +0900

    2025/08/14修正

--- src/App.java
+++ src/App.java
@@ -2,9 +2,12 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Scanner scanner = new Scanner(System.in); 
+        Scanner scanner = new Scanner(System.in,"Shift-JIS"); 
         System.out.print("タイトル： ");
-        String name = scanner.next();
-        System.err.println(name);
+        String title = scanner.next();
+        System.out.println(title);
+        System.out.print("つぶやき： ");
+        String tweet = scanner.next();
+        System.out.println(tweet);
     }
 }
