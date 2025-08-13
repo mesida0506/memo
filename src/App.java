5cb7728216270eb593cf8a26266c9e5cade7de4d
unknown <jp.gomugomuborgs@gmail.com>
2025-08-14 03:13:12 +0900

    first step

---
+++ src/App.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in); 
+        System.out.print("タイトル： ");
+        String name = scanner.next();
+        System.err.println(name);
+    }
+}
